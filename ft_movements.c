0fe896e8a4edc4fc310fe1bb9c474be46b1e8238
Cristina Homobono Fernandez <chomobon@c2r13s3.42madrid.com>
2025-01-31 14:48:32 +0100

    He añadido dos movimientos mas que son las rotaciones y el push

--- ft_movements.c
+++ ft_movements.c
@@ -6,7 +6,7 @@
 /*   By: chomobon <chomobon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 17:04:59 by chomobon          #+#    #+#             */
-/*   Updated: 2025/01/29 17:13:32 by chomobon         ###   ########.fr       */
+/*   Updated: 2025/01/31 14:46:02 by chomobon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,52 @@ void    sa(t_stack **stack)
     second = (*stack)->next;
     if (second != NULL)
     {
-        
+        (*stack)->next = second->next;
+        second->next = (*stack);
+        (*stack) = second;
+        ft_printf("sa\n");
+    }
+}
+
+// * Con esta orden vamos a pasar las cosas de una pila a otra, en caso de que
+// * pasemos el primer elemento de la pila b a la a imprime pa, en caso
+// * contrario (de a a b) imprime pb. Si la pila de origen esta vacia no hace
+// * nada.
+void push(t_stack **src, t_stack **dest, char stackdest)
+{
+    t_stack *aux;
+
+    aux = (*src);
+    (*src) = aux->next;
+    aux->next = (*dest);
+    (*dest) = aux;
+    if (stackdest == 'a')
+        ft_printf("pa\n");
+    else
+        ft_printf("pb\n");
+}
+
+// * Con esta orden lo que vamos a hacer es desplazar los elementos hacia
+// * arriba o hacia abajo, en caso de que se rotara hacia arriba lo que hace
+// * es ra y rota todos los elementos hacia arriba y el primer elemento pasa a
+// * ser el ultimo elemento. En caso de que fuera al reves podemos ver que 
+// * se rotan todos hacia abajo y el ultimo pasa a ser el primero (rb).
+void rotate(t_stack **stack, char option)
+{
+    t_stack *last;
+    t_stack *aux;
+
+    last = (*stack)->next;
+    if (last != NULL)
+    {
+        while (last->next)
+            last = last->next;
+        aux = (*stack);
+        last->next = aux;
+        aux ->next = NULL;
+        if (option = 'a')
+            ft_printf("ra\n");
+        else
+            ft_printf("rb\n");
     }
 }
